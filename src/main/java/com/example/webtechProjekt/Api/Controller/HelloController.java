fcc070d6000d61ac7cc4bc5d25f7d1943a8c499e
unknown <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-10-27 10:14:52 +0200

    updated project structure

--- src/main/java/com/example/webtechProjekt/Api/Controller/HelloController.java
+++ src/main/java/com/example/webtechProjekt/Api/Controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.example.webtechProjekt.Api.Controller;
+package com.example.webtechProjekt.api.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;

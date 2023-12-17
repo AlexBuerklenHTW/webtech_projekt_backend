b982d57048c7f8fc559da9074ff25f3168f41414
Alexander Bürklen <Alexander.Buerklen@Student.HTW-Berlin.de>
2023-12-17 16:52:57 +0100

    stackoverflowerror

--- src/main/java/com/example/webtechProjekt/controller/TripController.java
+++ src/main/java/com/example/webtechProjekt/controller/TripController.java
@@ -28,7 +28,7 @@ public class TripController {
     }
 
     @PostMapping("/trips/{tripId}/markers")
-    public Trip addTripMarkers(@RequestBody Trip theTrip, @PathVariable String tripId){
+    public Trip addTripMarkers(@RequestBody Trip theTrip){
 
         //theTrip.setId(0);
 

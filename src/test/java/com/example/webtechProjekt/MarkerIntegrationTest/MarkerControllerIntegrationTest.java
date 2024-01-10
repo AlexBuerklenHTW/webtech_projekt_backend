fdb99f7a9e39f24de25a62291478614707a54b2e
Alexander Bürklen <Alexander.Buerklen@Student.HTW-Berlin.de>
2024-01-10 20:34:00 +0100

    adapted dependencies for deployment

--- src/test/java/com/example/webtechProjekt/MarkerIntegrationTest/MarkerControllerIntegrationTest.java
+++ src/test/java/com/example/webtechProjekt/MarkerIntegrationTest/MarkerControllerIntegrationTest.java
@@ -14,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.jdbc.JdbcTestUtils;
 import org.springframework.web.client.RestTemplate;
@@ -22,6 +23,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 public class MarkerControllerIntegrationTest {
     @LocalServerPort
     private int port;
